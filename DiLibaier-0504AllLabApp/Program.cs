e204fb16ab8099174974f74e348886f8bfd32888
Administrator <Administrator@PC-202003111938>
2022-04-18 17:00:42 +0800

    添加项目文件。

---
+++ DiLibaier-0504AllLabApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiLibaier_0504AllLabApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first Number:");
+            double n1 =Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second Number:");
+            double n2=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter operator:");
+            string op =Console.ReadLine();
+            double result = 0;
+            bool valid = true;
+            switch(op)
+            {
+                case "+":
+                    result = n1 + n2;
+                    break;
+                    case "-":
+                    result = n1 - n2;
+                    break;
+                case "*":
+                    result = n1 * n2;
+                    break;
+                case "/":
+                    result = n1 / n2;
+                    break;
+            }
+            if (valid) Console.WriteLine("Result is:" + result);
+            else Console.WriteLine("Invalid Operator!");
+        }
+    }
+}
