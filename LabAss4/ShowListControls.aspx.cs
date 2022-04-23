4082c62e6dea276022032765424ff81791b0efdc
Administrator <Administrator@PC-202003111938>
2022-04-24 00:48:51 +0800

    添加lab4

---
+++ LabAss4/ShowListControls.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LabAss4
+{
+    public partial class ShowListControls : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] toms = { "Sofia", "Plovdiv", "Varna" };
+
+         /*   ListBox1.DataSource = toms;
+            ListBox1.DataBind();
+            DropDownList1.DataSource = toms;
+            DropDownList1.DataBind();
+            BulletedList1.DataSource = toms;
+            BulletedList1.DataBind();
+            CheckBoxList1.DataSource = toms;
+            CheckBoxList1.DataBind();
+            RadioButtonList1.DataSource = toms;
+            RadioButtonList1.DataBind();
+         */
+        }
+    }
+}
